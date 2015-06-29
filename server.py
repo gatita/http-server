85920f04bf86a5f899d90be75fb475d1d4c4493a
Nicholas Draper <ndraper2@gmail.com>
2015-06-29 16:00:06 -0700

    fixed header concatenation

--- server.py
+++ server.py
@@ -16,18 +16,18 @@ server.listen(1)
 def response_ok():
     now = formatdate(usegmt=True)
     body = "Hello World"
-    return 'HTTP/1.1 200 OK\r\n\
-        Date: {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\
-        Connection: close\r\n\r\nHello World'.format(now, len(body))
+    return ('HTTP/1.1 200 OK\r\n'
+        'Date: {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n'
+        'Connection: close\r\n\r\nHello World'.format(now, len(body)))
 
 
 def response_error():
     now = formatdate(usegmt=True)
     body = "<html><body>\nThe server encountered an unexpected internal \
     error.</body></html>"
-    return 'HTTP 1.1 500 Internal Server Error\
-        \r\nDate:{}\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\
-        Connection: close\r\n\r\n{}'.format(now, len(body), body)
+    return ('HTTP 1.1 500 Internal Server Error'
+        '\r\nDate:{}\r\nContent-Type: text/html\r\nContent-Length: {}\r\n'
+        'Connection: close\r\n\r\n{}'.format(now, len(body), body))
 
 
 while True:
