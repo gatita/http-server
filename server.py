7ccd3b1fb503dd8c47ae986d7ed7694b16086db4
Nicholas Draper <ndraper2@gmail.com>
2015-06-29 16:31:12 -0700

    unit tests for responses work

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ server.listen(1)
 
 def response_ok():
     now = formatdate(usegmt=True)
-    body = "Hello World"
+    body = "Hello World, I need to travel."
     return ('HTTP/1.1 200 OK\r\n'
         'Date: {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n'
         'Connection: close\r\n\r\n{}'.format(now, len(body), body))
@@ -23,23 +23,24 @@ def response_ok():
 
 def response_error():
     now = formatdate(usegmt=True)
-    body = "<html><body>\nThe server encountered an unexpected internal \
-    error.</body></html>"
+    body = ('<html><body>\nThe server encountered an unexpected internal '
+    'error.</body></html>')
     return ('HTTP 1.1 500 Internal Server Error'
         '\r\nDate:{}\r\nContent-Type: text/html\r\nContent-Length: {}\r\n'
         'Connection: close\r\n\r\n{}'.format(now, len(body), body))
 
-
-while True:
-    try:
-        conn, addr = server.accept()
-        msg = ""
-        while True:
-            msg = msg + conn.recv(16)
+if __name__ == '__main__':
+    while True:
+        try:
+            conn, addr = server.accept()
+            message_in = ""
+            while True:
+                msg = conn.recv(16)
+                message_in += msg
+                if len(msg) < 16:
+                    break
             conn.sendall(response_ok())
-            if len(msg) < 16:
-                conn.close()
-                break
-        print msg
-    except KeyboardInterrupt:
-        break
+            conn.close()
+            print message_in
+        except KeyboardInterrupt:
+            break
--- test_http_server.py
+++ test_http_server.py
@@ -1,6 +1,6 @@
 # -*- coding:UTF-8 -*-
 from __future__ import unicode_literals
-import server
+from server import response_error, response_ok
 from email.utils import formatdate
 
 import pytest
@@ -8,18 +8,18 @@ import pytest
 
 def test_response_ok():
     now = formatdate(usegmt=True)
-    body = "Hello World"
-    assert server.response_ok() == ('HTTP/1.1 200 OK\r\n\
-        Date: {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\
-        Connection: close\r\n\r\nHello World'.format(now, len(body))
+    body = "Hello World, I need to travel."
+    assert response_ok() == ('HTTP/1.1 200 OK\r\n'
+        'Date: {}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n'
+        'Connection: close\r\n\r\n{}'.format(now, len(body), body)
     )
 
 
 def test_response_error():
     now = formatdate(usegmt=True)
-    body = "<html><body>\nThe server encountered an unexpected internal \
-    error.</body></html>"
-    assert server.response_error() == ('HTTP 1.1 500 Internal Server Error\
-        \r\nDate:{}\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\
-        Connection: close\r\n\r\n{}'.format(now, len(body), body)
+    body = ("<html><body>\nThe server encountered an unexpected internal "
+    "error.</body></html>")
+    assert response_error() == ('HTTP 1.1 500 Internal Server Error'
+        '\r\nDate:{}\r\nContent-Type: text/html\r\nContent-Length: {}\r\n'
+        'Connection: close\r\n\r\n{}'.format(now, len(body), body)
     )
